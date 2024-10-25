27692ca9fbb572b9c4848cfabc1f8737b6da4d5e
adria <adrianzu14@gmail.com>
2024-10-25 09:22:01 +0200

    comit 3

--- code.js
+++ code.js
@@ -7,7 +7,7 @@ window.onload = async () => {
         const mainHtmlElement = document.getElementById('ulPokemon');
         const newElement = document.createElement('li');
 
-        newElement.innerHTML = `<h2>${poke.name}</h2><p></p>`
+        newElement.innerHTML = `<h2 onclick="infoPoke(${poke.id})">${poke.name}</h2><p></p>`
 
         if (poke.name !== null){
             mainHtmlElement.appendChild(newElement);
@@ -23,8 +23,24 @@ async function getPokemonKanto(){
 }
 
 async function getPokeById(pokeId){
-    const response = await fetch
+    const response = await fetch(`${POKEMON_BASE_URL}pokemon/${pokeId}`);
     const data = await response.json();
-    return data;
+    return data.results;
+}
+
+async function infoPoke(pokeId) {
+    document.getElementById('poke').style.display = 'block';
+    const sectionHtmlElement = document.getElementById('poke');
+    sectionHtmlElement.innerHTML = '';
+    const poke = await getPokeById(pokeId);
+  
+    const newElement = document.createElement('div');
+    newElement.innerHTML = `
+    <h1>${poke.name}</h1>
+    <p>${poke.id}</p>
+    <p>${poke.types.type.name}</p>`;
+  
+    sectionHtmlElement.appendChild(newElement);
   }
 
+  
