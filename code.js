9963741d90e497775f048ffa67bb9c0e412ff816
adria <adrianzu14@gmail.com>
2024-10-25 09:09:15 +0200

    commit 2

--- code.js
+++ code.js
@@ -4,16 +4,27 @@ window.onload = async () => {
     const pokemon = await getPokemonKanto();
 
     for (const poke of pokemon){
-        const mainHtmlElement = document.getElementById('pokemon');
-        const newElement = document.createElement('div');
+        const mainHtmlElement = document.getElementById('ulPokemon');
+        const newElement = document.createElement('li');
 
-        newElement.innerHTML =
+        newElement.innerHTML = `<h2>${poke.name}</h2><p></p>`
+
+        if (poke.name !== null){
+            mainHtmlElement.appendChild(newElement);
+
+        }
     }
 }
 
 async function getPokemonKanto(){
     const response = await fetch(`${POKEMON_BASE_URL}pokemon/?limit=151`);
     const data = await response.json();
-    return data;
+    return data.results;
 }
-  
+
+async function getPokeById(pokeId){
+    const response = await fetch
+    const data = await response.json();
+    return data;
+  }
+
